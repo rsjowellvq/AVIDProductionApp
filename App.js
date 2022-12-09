954b159c3b9e6faee1c9be53ab0d9290474930f7
Russell Jowell <russell.jowell@VQ-C64253-MacBook-Pro.local>
2022-12-09 17:19:04 -0500

    added new files

--- App.js
+++ App.js
@@ -5,8 +5,8 @@
  * @format
  * @flow strict-local
  */
-
-import React from 'react';
+import 'react-native-gesture-handler';
+import React, { useState } from 'react';
 //import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -21,6 +21,8 @@ import {
   View,
 } from 'react-native';
 import BleManager, { read } from 'react-native-ble-manager';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Buffer from 'buffer';
 import {
   Colors,
@@ -29,6 +31,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { title } from 'process';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
@@ -61,21 +64,62 @@ import {
 const bleManagerModule = NativeModules.BleManager;
 console.log("Cheeseburger1 "+bleManagerModule);
 const bleManagerEmitter = new NativeEventEmitter(bleManagerModule);
+const Stack = createStackNavigator();
 const deviceList = [];
+var deviceData = [];
 const buttonList = [];
+console.log("Step 1");
+
+console.log("Step 2");
 //const hexLetters = ['A','B','C','D','E','F'];
-//const firstAddresses = ['00','14','28','3C']
+const firstAddresses = ['00','14','28','3C','50','64','78','8C','A0','B4','C8','DC','F0'];
 //let command = ['00','00'];
 let addressData = ['00','0A'].map(x=>{return parseInt(x,16);});
 
 
-
-
-
 const App = () => {
+
+  return(
+
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoadScreen">
+        <Stack.Screen name="LoadScreen" component={LoadScreen}  />
+        <Stack.Screen name="SetScreen" component={SetScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  
+   );
+
+
+}
+
+const SetScreen = ({route,navigation}) => {
+
+  const {serialNumber,UUID,FirstBytes} = route.params;
   
   
+  return(
+    <View>
+      <Text>{serialNumber}</Text>
+      <Text>{UUID}</Text>
+      <Text>{FirstBytes}</Text>
+    </View>
+
+
+
+  );
+
+
+}
+
+
+const LoadScreen = ({navigation}) => {
   
+  
+  const [buttons,setButtons] = React.useState([]);
+  const buttonsRef = React.useRef(buttons);
+  
+
   React.useEffect(()=>{
 
     BleManager.start();
@@ -109,113 +153,236 @@ const App = () => {
 
   }
     
-    
+  
+  function updateButtonList()
+  {
+  
+  React.useEffect(()=>{
+
+    console.log("Here We Go");
+    setButtons(buttonList);
+    console.log("Winning");
+
+
+  },[buttonList]);
+
+  }
+
+
+  
+
+
   const handleStopScan = () => {
 
-    var currentVal = 0;
-
-    for(var i=0;i<20;i++)
+   
+    var currentVal = "";
+    var currentHexIndex = 0;
+      
+    
+    function writeAndRead256Data(inputDevice)
     {
-      currentVal += 20;
+      console.log("The Current Thing Is "+inputDevice.id);
+      let addressData = ['00',firstAddresses[currentHexIndex]].map(x=>{return parseInt(x,16);});
+      
+      setTimeout(()=>{
+
+        BleManager.write(inputDevice.id,"F0001130-0451-4000-B000-000000000000","F0001131-0451-4000-B000-000000000000", addressData).then(()=>{
+
+          setTimeout(()=>{
+  
+            BleManager.read(inputDevice.id,"F0001130-0451-4000-B000-000000000000","F0001132-0451-4000-B000-000000000000").then((readData)=>{
+  
+              console.log("Data From Device 2aa "+readData);
+              const buffer = Buffer.Buffer.from(readData); //https://github.com/feross/buffer#convert-arraybuffer-to-buffer
+              //const sensorData = buffer.readUInt8(7, true);
+              for(var i = 0;i<20;i++)
+              {
+                currentVal+=buffer.readUInt8(i).toString(16);
+              }
+              console.log("The String Is "+currentVal);
+              //console.log("Buffer Data 4 "+sensorData);
+              //currentVal += String(readData);
+              if(currentHexIndex < 12)
+              {
+                currentHexIndex+=1;
+                console.log("Phishing");
+                
+                writeAndRead256Data(inputDevice);
+                  
+              }
+              else
+              {
+                deviceData.push(currentVal);
+                //console.log("Serial Num Is "+currentVal.substring(167,173));
+                buttonList.push(<Button title={currentVal.substring(167,173)} ref={ref => this.button = ref} deviceUUID={inputDevice.id} key={currentVal} nativeID={currentVal}  onPress={()=>{navigation.navigate('SetScreen',{serialNumber:this.button.props.title,UUID:this.button.props.deviceUUID,FirstBytes:this.button.props.nativeID})}} />  );
+                console.log("How many buttons "+buttonList.length+buttonList[0].accessibilityLabel);
+                currentVal = "";
+                currentHexIndex = 0;
+                setButtons(buttonList);
+                
+                return;
+              }
+  
+              //const buffer = Buffer.Buffer.from(readData); //https://github.com/feross/buffer#convert-arraybuffer-to-buffer
+              //const sensorData = buffer.readUInt8(, true);
+              //console.log("Buffer Data 2 "+sensorData);
+    
+    
+            });
+           
+          },100);
+  
+        });
+
+
+      },100);
+      
+      
+     
     }
-    
-    
-    
-    
-    
-    
-    for(var i = 0;i<deviceList.length;i++)
+
+    var deviceCount = 0;
+
+    function getDeviceData()
     {
       
-      let currentDevice = deviceList[i];
-      
-      BleManager.connect(deviceList[i].id).then(()=>{
+      console.log("Device This "+deviceList[deviceCount].id);
 
-        BleManager.retrieveServices(currentDevice.id,["F0001130-0451-4000-B000-000000000000"]).then((deviceInfo)=>{
+      BleManager.connect(deviceList[deviceCount].id).then((value)=>{
 
-          BleManager.write(currentDevice.id,"F0001130-0451-4000-B000-000000000000","F0001131-0451-4000-B000-000000000000", addressData).then(()=>{
+        console.log("Pizza Pizza");
 
-            console.log("Write Success2233");
+        BleManager.retrieveServices(deviceList[deviceCount].id,["F0001130-0451-4000-B000-000000000000"]).then((deviceInfo)=>{
 
+          console.log("Cheese Cheese");
+          
+          writeAndRead256Data(deviceList[deviceCount]);
+
+            deviceCount += 1;
+          if(deviceCount < deviceList.length)
+          {
+          
+            console.log("Russia");
+            getDeviceData();
+          }
+          else
+          {
+            console.log("The Length Is "+buttonList.length);
             
-            setTimeout(()=>{
+          }
 
-              BleManager.read(currentDevice.id,"F0001130-0451-4000-B000-000000000000","F0001132-0451-4000-B000-000000000000").then((readData)=>{
-    
-                console.log("Data From Device 2aa "+readData);
-    
-                //const buffer = Buffer.Buffer.from(readData); //https://github.com/feross/buffer#convert-arraybuffer-to-buffer
-                //const sensorData = buffer.readUInt8(, true);
-                //console.log("Buffer Data 2 "+sensorData);
-      
-      
-              });
-             
-            },100);
-            /*
-  
-            BleManager.read(currentDevice.id,"F0001130-0451-4000-B000-000000000000","F0001132-0451-4000-B000-000000000000").then((readData)=>{
-    
-              console.log("Data From Device 333 "+readData.data);
-  
-              const buffer = Buffer.Buffer.from(readData); //https://github.com/feross/buffer#convert-arraybuffer-to-buffer
-              const sensorData = buffer.readUInt8(7, true);
-              console.log("Buffer Data 3 "+sensorData);
-    
-    
-            });
 
-            BleManager.read(currentDevice.id,"F0001130-0451-4000-B000-000000000000","F0001132-0451-4000-B000-000000000000").then((readData)=>{
-    
-              console.log("Data From Device 444 "+readData.data);
-  
-              const buffer = Buffer.Buffer.from(readData); //https://github.com/feross/buffer#convert-arraybuffer-to-buffer
-              const sensorData = buffer.readUInt8(7, true);
-              console.log("Buffer Data 4 "+sensorData);
-    
-    
-            });
-
-            BleManager.read(currentDevice.id,"F0001130-0451-4000-B000-000000000000","F0001132-0451-4000-B000-000000000000").then((readData)=>{
-    
-              console.log("Data From Device 555 "+readData.data);
-  
-              const buffer = Buffer.Buffer.from(readData); //https://github.com/feross/buffer#convert-arraybuffer-to-buffer
-              const sensorData = buffer.readUInt8(7, true);
-              console.log("Buffer Data 5 "+sensorData);
-    
-    
-            });
-
-            BleManager.read(currentDevice.id,"F0001130-0451-4000-B000-000000000000","F0001132-0451-4000-B000-000000000000").then((readData)=>{
-    
-              console.log("Data From Device 666 "+readData);
-  
-              const buffer = Buffer.Buffer.from(readData); //https://github.com/feross/buffer#convert-arraybuffer-to-buffer
-              const sensorData = buffer.readUInt8(7, true);
-              console.log("Buffer Data 6 "+sensorData);
-    
-    
-            });*/
-            
-  
-          });
 
           
+        //
+          console.log("Cheeseburger");
+        
+        //updateButtonList();
+        //buttonsRef.current = buttonList;
+        
+        
+        
+
+        console.log("Hamburger");
+        
+        
+        console.log("Buttons Length Is "+buttons.length);
+
+      
+       
+      });
+
+
+    });
+
+
+
+
+
+      
+      
+      
+      
+      
+      
+      /*
+      for(var i = 0;i<deviceList.length;i++)
+      {
+      
+          let currentDevice = deviceList[i];
+      
+          BleManager.connect(deviceList[i].id).then(()=>{
+
+          BleManager.retrieveServices(currentDevice.id,["F0001130-0451-4000-B000-000000000000"]).then((deviceInfo)=>{
+
+          writeAndRead256Data(currentDevice);
+          //
+          console.log("Cheeseburger");
+          
+          //updateButtonList();
+          //buttonsRef.current = buttonList;
+          
+          
+          
+          
+
+          console.log("Hamburger");
+          
+          
+          console.log("Buttons Length Is "+buttons.length);
+
+        
          
-
-          
-
-
         });
 
 
       });
       
       
+      
+      
       //console.log("The ID Is "+device[i].id);
+      }*/
+      console.log("The Num of Buttons "+buttonList.length);
+
+      return new Promise((resolve,reject)=>{
+
+        resolve(true);
+        
+        if(1==0)
+        {
+          reject(false);
+        }
+
+      });
+
+
     }
 
+    getDeviceData().then((result)=>{
+
+      console.log("Vanilla "+buttonList.length);
+      buttonsRef.current = buttonList;
+      console.log("Chocolate "+buttons.length);
+
+
+    }).catch((error)=>{console.log("Error");});
+
+    function setNewButtons()
+    {
+      
+    }
+    
+    
+    
+    
+  
+
+    
+   
+
+
+    
 
   };
     
@@ -236,53 +403,35 @@ const App = () => {
 
   }
 
-  function retrieveSerialNumber(device)
-  {
-    BleManager.stopScan().then(()=>{
+ 
+  
+  
+  
+  console.log("Num button is "+buttons.length);
 
-      BleManager.connect(device.id).then(()=>{
+  const handleButtonClick = (e) => {
 
+    console.log("TAADAA");
+    console.log(e.props.title);
 
-
-
-
-      });
-
-
-
-    });
   }
-  
-  
-  
-
-  /*
-
-      React.useEffect(()=>{
-
-      BleManager.start();
-
-      bleManagerEmitter.addListener('BleManagerDiscoverPeripheral',handleDiscoverDevice);
-
-      BleManager.scan([],30,false).then(()=>{
-
-        console.log("Scan Started");
-
-      });
 
 
-  */
 
-
+ 
 
 
 
   return (
     <View>
-      <Text>Home Screen</Text>
-      <Text>Home Screen</Text>
-      <Text>Home Screen</Text>
-      <Button onPress={scanForAvidDevices} title="Start Scan"/>
+      <Text>&nbsp;</Text>
+      <Text>&nbsp;</Text>
+      <Text>&nbsp;</Text>
+      <Button titleStyle={{fontSize: 25}} onPress={scanForAvidDevices} title="Start Scan"/>
+      <ScrollView>
+        
+        {buttons}
+      </ScrollView>
     </View>
   );
 };
